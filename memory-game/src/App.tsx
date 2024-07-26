6cec86b8b00d7c4fc1a7dfd1ec540f5d423a5827
Arber Shaska <arbershaska2@gmail.com>
2024-07-26 23:53:09 +0200

    First Commit

---
+++ memory-game/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const arr = ["A","A","B","B","C","C","D","D","E","E","F","F","G","G","H","H","I","I","J","J","K","K","L","L"]
+  console.log(arr)
+
+  return (
+    <>
+     <h1 className='text-red-500'>Hello</h1>
+    </>
+  )
+}
+
+export default App
